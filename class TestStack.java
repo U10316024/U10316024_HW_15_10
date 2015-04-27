99d5ff64cc35d16a2c74baded03422165b2cf4a4
U10316024 <gchew0006078@gmail.com>
2015-04-27 21:24:25 +0800

    Update class TestStack.java
    這是經過改編的測試程式
    創造兩個物件stack1以及stack2
    在裡面輸入字串
    並且創造stack3深層複寫stack2
    最後印出顛倒的字串

--- class TestStack.java
+++ class TestStack.java
@@ -6,18 +6,26 @@ public class TestStack {
 	public static void main(String[] args) {
 		MyStack stack1 = new MyStack();
 
-		//輸入三個字串
-		stack1.push("String1");
-		stack1.push("String2");
-		stack1.push("String3");
+			//輸入字串
+			stack1.push("!");
+			stack1.push("Y");
+			stack1.push("A");
+			stack1.push("D");
+			stack1.push("H");
+			stack1.push("T");
+			stack1.push("R");
+			stack1.push("I");
+			stack1.push("\nB");
 
 		//stack2 deep copy stack1
 		MyStack stack2 = (MyStack)stack1.clone();
 	
-		//輸入三個字串
-		stack2.push("String1");
-		stack2.push("String2");
-		stack2.push("String3");
+			//輸入字串
+			stack2.push("Y");
+			stack2.push("P");
+			stack2.push("P");
+			stack2.push("A");
+			stack2.push("H");
 
 		//print stack1 的 ArrayList size
 		System.out.println("stack1 : " + stack1.getSize());
@@ -25,5 +33,12 @@ public class TestStack {
 		//print stack2 的 ArrayList size
 		//因為stack2 deep copy stack1 所以stack2的ArrayList的size為stack1+stack2
 		System.out.println("stack2 : " + stack2.getSize());
+
+		MyStack stack3 = (MyStack)stack2.clone();
+		
+		//將stack1+stack2的字串顛倒印出
+		while (!(stack2.isEmpty())) {
+			System.out.println(stack3.pop());
+		}				
 	}
 }
